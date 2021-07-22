2c54c8aaf47038f79565e526893b8b41683356b6
sergigt10 <phxhollow13@hotmail.com>
2021-07-22 19:31:15 +0200

    analytics v2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import "../styles/globals.css";
 
-export default MyApp
+const App = ({ Component, pageProps }) => {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
+	return <Component {...pageProps} />;
+};
+
+export default App;

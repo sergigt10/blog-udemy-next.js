e1f87ec990e877d66d569bcf29266d9dcbaa63bb
Sergi Generó <phxhollow13@hotmail.com>
2021-11-01 21:10:26 +0100

    news 01/11

--- components/CategoryLabel.js
+++ components/CategoryLabel.js
@@ -2,27 +2,27 @@ import { removeAccents } from "@/utils/clearCategory";
 import Link from "next/link";
 
 export default function CategoryLabel({ children }) {
-	const colorKey = {
-		Accesorios: "Blue",
-		Mantenimiento: "Green",
-		Ropa: "Purple",
-		Reparación: "Red",
-		Electrónica: "Yellow",
-		Zapatillas: "Gray",
-		Componentes: "Pink",
-		Nutrición: "Indigo",
-	};
+    const colorKey = {
+        Accesorios: "Blue",
+        Mantenimiento: "Green",
+        Ropa: "Purple",
+        Reparación: "Red",
+        Electrónica: "Yellow",
+        Zapatillas: "Gray",
+        Componentes: "Pink",
+        Nutrición: "Indigo",
+    };
 
-	return (
-		<div
-			className={`px-2 py-1 bg-category${colorKey[children]} text-white rounded`}
-		>
-			{/* bg-${colorKey[children]}-600 */}
-			<Link
-				href={`/ofertas-mtb-2021/categoria/${removeAccents(children)}`}
-			>
-				{children}
-			</Link>
-		</div>
-	);
+    return (
+        <div
+            className={`px-2 py-1 bg-category${colorKey[children]} text-white rounded font-bold`}
+        >
+            {/* bg-${colorKey[children]}-600 */}
+            <Link
+                href={`/ofertas-mtb-2021/categoria/${removeAccents(children)}`}
+            >
+                {children}
+            </Link>
+        </div>
+    );
 }
